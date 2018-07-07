e5539e18d8f43f7e7859d138948b5308910dada7
Kshitij Mani Parashar <kshitijmaniparashar@qait.com>
2018-07-07 19:06:32 +0530

    First file

---
+++ Arraymin.java
@@ -0,0 +1,21 @@
+class Arraymin
+{
+	public static void main(String args[])
+	{
+		int i,min;
+		int arr[]=new int[4];
+		arr[0]=3;
+		arr[1]=2;
+		arr[2]=1;
+		arr[3]=4;	
+		min=arr[0];
+		for(i=1;i<arr.length;i++)
+		{
+			if(min>arr[i])
+			{
+				min=arr[i];
+			}
+		}
+		System.out.println("Minimum element is "+min);
+	}
+}
